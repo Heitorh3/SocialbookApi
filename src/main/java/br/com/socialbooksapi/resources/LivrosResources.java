543e14623f9a4d97531454dc369e80e5efb66891
Heitor Carvalho de Almeida Neto <Heitor Carvalho de Almeida Neto>
2016-03-24 10:25:15 -0300

    Deletando um registro <Livro> utilizando o methodo DELETE e @PathVariable

--- src/main/java/br/com/socialbooksapi/resources/LivrosResources.java
+++ src/main/java/br/com/socialbooksapi/resources/LivrosResources.java
@@ -34,4 +34,9 @@ public class LivrosResources {
 	public Livro buscar(@PathVariable("id") Long id){
 		return livrosRepository.findOne(id);
 	}
+	
+	@RequestMapping(value = "/{id}",  method = RequestMethod.DELETE)
+	public void deletar(@PathVariable("id") Long id){
+		livrosRepository.delete(id);
+	}
 }
