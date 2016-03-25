3c75e812b149818abc7f6afac55f4207ae1aa5c6
Heitor Carvalho de Almeida Neto <heitorh3@gmail.com>
2016-03-25 14:36:48 -0300

    Finalizando as tratativas de retorno seguindoas convecoes HTTP

--- src/main/java/br/com/socialbooksapi/resources/LivrosResources.java
+++ src/main/java/br/com/socialbooksapi/resources/LivrosResources.java
@@ -3,6 +3,9 @@ package br.com.socialbooksapi.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,8 +24,8 @@ public class LivrosResources {
 	
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public List<Livro> listar(){
-		return livrosRepository.findAll();
+	public ResponseEntity<List<Livro>> listar(){
+		return ResponseEntity.status(HttpStatus.OK).body(livrosRepository.findAll());
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
@@ -36,13 +39,20 @@ public class LivrosResources {
 	}
 	
 	@RequestMapping(value = "/{id}",  method = RequestMethod.DELETE)
-	public void deletar(@PathVariable("id") Long id){
-		livrosRepository.delete(id);
+	public ResponseEntity<Void> deletar(@PathVariable("id") Long id){
+		try{
+			livrosRepository.delete(id);
+		}catch(EmptyResultDataAccessException e){
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.noContent().build();
 	}
 	
 	@RequestMapping(value = "/{id}",  method = RequestMethod.PUT)
-	public void atualizar(@RequestBody Livro livro, @PathVariable("id") Long id){
+	public ResponseEntity<Void> atualizar(@RequestBody Livro livro, @PathVariable("id") Long id){
 		livro.setId(id);
-		livrosRepository.save(livro);		
+		livrosRepository.save(livro);	
+		
+		return ResponseEntity.noContent().build();
 	}
 }
