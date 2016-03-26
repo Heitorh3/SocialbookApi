e3c52bc8ebe2cc965333bf00ff5543cf752e5f18
Heitor Carvalho de Almeida Neto <heitorh3@gmail.com>
2016-03-26 10:43:53 -0300

    Adicionando comentários ao recurso Livro

--- src/main/java/br/com/socialbooksapi/resources/LivrosResources.java
+++ src/main/java/br/com/socialbooksapi/resources/LivrosResources.java
@@ -1,5 +1,6 @@
 package br.com.socialbooksapi.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +11,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import br.com.socialbooksapi.domain.Comentario;
 import br.com.socialbooksapi.domain.Livro;
 import br.com.socialbooksapi.service.LivrosService;
 
@@ -51,5 +54,15 @@ public class LivrosResources {
 		livrosService.atualizar(livro);
 		
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/{id}/comentarios", method = RequestMethod.POST)
+	public ResponseEntity<Void> adcinarComentario(@PathVariable("id") Long livroId, @RequestBody Comentario comentario){
+		
+		livrosService.salvarComentario(livroId, comentario);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().build()
+				.toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
